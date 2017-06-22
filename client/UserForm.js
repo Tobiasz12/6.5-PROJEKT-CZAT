562273d18259093d9c982a67df581b6b22c9e10d
Tobiasz12 <tobiasz63@op.pl>
2017-06-22 18:22:23 +0200

    moduł 6

---
+++ client/UserForm.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+import styles from './css/UserForm.css';
+
+class UserForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {name: ''};
+        }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        this.props.onUserSubmit(this.state.name);
+    }
+
+    handlechange(e) {
+        this.setState({name: e.target.value});
+    }
+
+    render() {
+        return(
+            <form className={styles.USerForm} onSubmit={e => this.handleSubmit(e)}>
+                <input
+                    className={styles.UserInput}
+                    placeholder='Write your nickname and press enter'
+                    onChange={e => this.handlechange(e)}
+                    value={this.state.name}
+                />
+            </form>
+        );
+    }
+}
+
+export default UserForm;
